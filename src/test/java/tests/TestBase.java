39a286896cbd8ded8fc8eb36c98902eba7500161
IrinaLerner <irina.bliudo14@gmail.com>
2021-11-15 23:30:40 +0200

    screenshot

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,7 +3,9 @@ package tests;
 import manager.ApplicationManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
@@ -22,4 +24,14 @@ public class TestBase {
 
         app.stop();
     }
+   /* @BeforeMethod
+    public void startTest(Method m){
+        logger.info("Start test ---> "+m.getName());
+    }
+    @AfterMethod
+    public void stopTest(Method m){
+        logger.info("The end of test ---> "+m.getName());
+    }
+
+    */
 }
